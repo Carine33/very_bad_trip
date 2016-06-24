1456099aee18c05bc6640784095c3ca0b1cb467d
damienVermilas <d.vermilas@hotmail.fr>
2016-06-24 16:49:36 +0200

    route fonctionnel

--- app/routes.php
+++ app/routes.php
@@ -5,8 +5,6 @@
 		['GET', '/', 'Default#home', 'default_home'],
 		['GET|POST', '/page-quiz', 'Quiz#repondreQuiz', 'quiz_repondreQuiz'],
 		['GET', '/insertion', 'Insert#insertDb', 'insert_db'],
-		['GET|POST', '/page-admin', 'Users#selectUsers', 'users_selectUsers'],
-		['GET', '/page-supprimer-user/[i:userid]', 'Users#supprimerUser', 'user_supprimerUser'],
 		['GET|POST', '/inscription', 'Signin#signin', 'signin_signin'],
 		['GET|POST', '/page-admin-user', 'Users#selectUsers', 'users_selectUsers'],
 		['GET|POST', '/page-admin-dest', 'Users#selectDestinations', 'users_selectDestinations'],
