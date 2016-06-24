283a65fe11eadc5d93361cc3b438d8b7df8429e9
Hugues0111 <hugues.fonteyraud@wanadoo.fr>
2016-06-24 16:33:44 +0200

    routes

--- app/routes.php
+++ app/routes.php
@@ -4,7 +4,8 @@
 		['GET', '/', 'Default#home', 'default_home'],
 		['GET|POST', '/page-quiz', 'Quiz#repondreQuiz', 'quiz_repondreQuiz'],
 		['GET', '/insertion', 'Insert#insertDb', 'insert_db'],
-		['GET|POST', '/page-admin', 'Users#selectUsers', 'users_selectUsers'],
+		['GET|POST', '/page-admin-user', 'Users#selectUsers', 'users_selectUsers'],
+		['GET|POST', '/page-admin-dest', 'Users#selectDestinations', 'users_selectDestinations'],
 		['GET', '/page-supprimer-user/[i:userid]', 'Users#supprimerUser', 'user_supprimerUser'],
-
+		['GET', '/page-supprimer-destination/[i:destinationid]', 'Users#supprimerDestination', 'user_supprimerDestination'],
 	);
