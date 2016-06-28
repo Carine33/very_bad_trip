1a445cc18f1346fd98fd33ef40b3b9ad2b30d45d
Hugues0111 <hugues.fonteyraud@wanadoo.fr>
2016-06-28 14:05:05 +0200

    modif formulaire lostpassword

--- app/Views/login/lostpassword.php
+++ app/Views/login/lostpassword.php
@@ -141,6 +141,7 @@ if(!empty($params['showFormPassword'])){$showFormPassword = $params['showFormPas
                             
                         </div>
                     </div>
+                </div>
             </form>
         <?php endif; // ferme le if/else de $linkChangePassword ?>
     <?php endif; ?>
@@ -151,24 +152,21 @@ if(!empty($params['showFormPassword'])){$showFormPassword = $params['showFormPas
             <input type="hidden" name="action" value="updatePassword">
             <input type="hidden" name="email" value="<?=$_GET['email'];?>">
             <input type="hidden" name="token" value="<?=$_GET['token'];?>">
-            <div class="form-group">
-                <label class="col-md-4 control-label" for="new_password">Votre nouveau mot de passe</label>
-                <div class="col-md-4">
-                    <input type="password" name="new_password" id="new_password" class="form-control">
-                </div>
-            </div>
-
-            <div class="form-group">
-                <label class="col-md-4 control-label" for="new_password_conf">Confirmation du mot de passe</label>
-                <div class="col-md-4">
-                    <input type="password" name="new_password_conf" id="new_password_conf" class="form-control">
-                </div>
-            </div>
-
-            <div class="form-group">
-                <div class="col-md-4 col-md-offset-4">
-                    <button type="submit" class="btn btn-default">Mettre à jour mon mot de passe</button>
-                </div>
+            <div class="row">
+                    <div class="col-md-4 col-sm-6"></div>
+                    <div class="col-md-4 col-sm-6">
+                        <div class="form-group">
+                            <label for="new_password">Votre nouveau mot de passe</label>
+                            <input type="password" name="new_password" id="new_password" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="new_password_conf">Confirmation du mot de passe</label>
+                            <input type="password" name="new_password_conf" id="new_password_conf" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-xl">Mettre à jour mon mot de passe</button>
+                        </div>
+                    </div>
             </div>
         </form> 
     <?php endif; ?>
