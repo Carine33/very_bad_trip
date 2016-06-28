96d6abada5193ee73c65420ee7f42404f110226c
Hugues0111 <hugues.fonteyraud@wanadoo.fr>
2016-06-28 11:18:08 +0200

    page liste des des user et admin

--- app/Views/users/page_admin_users.php
+++ app/Views/users/page_admin_users.php
@@ -1,28 +1,45 @@
 <?php $this->layout('layoutAdmin', ['title' => 'Page d\'administration']) ?>
 
 <?php $this->start('main_content') ?>
-	<h1>page d'administration gestion des utilisateurs</h1>
+<div class="container">
+	<h1>gestion des utilisateurs</h1>
 	
 
-<table>
-	<tr>
-<td class="adminKey">avatar</td><td class="adminKey">nom</td><td class="adminKey">mot de passe</td><td class="adminEmail">email</td><td class="adminRole">role</td><td class="adminKey">pays</td><td class="adminKey">ville</td><td></td><td></td>
-</tr>
-<?php 
-foreach ($listeUsers as $user) {
-	# code...
+		<table>
+			<tr>
+				<td class="adminKey">avatar</td>
+				<td class="adminKey">nom</td>
+				<td class="adminEmail">email</td>
+				<td class="adminRole">role</td>
+				<td class="adminKey">pays</td>
+				<td class="adminKey">ville</td>
+				<td></td>
+				<td></td>
+			</tr>
+		<?php 
+		foreach ($listeUsers as $user) {
+			# code...
 
-		
+				
+
+					?>
+					<tr>
+						<td class="adminKey"><?= $user['avatar'] ?></td>
+						<td class="adminKey"><?= $user['username'] ?></td>
+						<td class="adminEmail"><?= $user['email'] ?></td>
+						<td class="adminRole"><?= $user['role'] ?></td>
+						<td class="adminKey"><?= $user['country'] ?></td>
+						<td class="adminKey"><?= $user['city'] ?></td>
+						<td class="adminKey"><a href="<?=$this->url('signin_selectSignin',['userid' => $user['id']]);?>">modifier</a></td>
+						<td class="adminKey"><a href="<?=$this->url('user_supprimerUser',['userid' => $user['id']]);?>">supprimer</a></td>
+					</tr>
+
+					<?php
+
+		}
+
 
 			?>
-			<tr><td class="adminKey"><?= $user['avatar'] ?></td><td class="adminKey"><?= $user['username'] ?></td><td class="adminKey"><?= $user['password'] ?></td><td class="adminEmail"><?= $user['email'] ?></td><td class="adminRole"><?= $user['role'] ?></td><td class="adminKey"><?= $user['country'] ?></td><td class="adminKey"><?= $user['city'] ?></td>
-			<td class="adminKey"><a href="<?=$this->url('signin_selectSignin',['userid' => $user['id']]);?>">modifier</a></td><td class="adminKey"><a href="<?=$this->url('user_supprimerUser',['userid' => $user['id']]);?>">supprimer</a></td></tr>
-
-			<?php
-
-}
-
-
-	?>
-	</table>
+		</table>
+</div>
 <?php $this->stop('main_content') ?>
